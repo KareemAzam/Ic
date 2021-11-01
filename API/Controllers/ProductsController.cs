ef1367af21c9521dcaa592b528f262bb290cbab6
Kareem <75427112+KareemAzam@users.noreply.github.com>
2021-11-01 03:11:50 +0200

    Initial Commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly Db _db;
+
+        public ProductsController(Db db)
+        {
+            _db = db;
+        }
+
+        // GET: api/<Products>
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _db.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        // GET api/<Products>/5
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _db.Products.FindAsync(id);
+        }
+
+        // POST api/<Products>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<Products>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<Products>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Infrastructure/Data/Db.cs
@@ -0,0 +1,15 @@
+﻿
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class Db:DbContext
+    {
+        public Db(DbContextOptions options) :base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
