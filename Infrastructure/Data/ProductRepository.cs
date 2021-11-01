333f6d08dedf331e233388af88f9ea25d771d137
Kareem <75427112+KareemAzam@users.noreply.github.com>
2021-11-01 04:41:29 +0200

    Adding a Repository and Interface

---
+++ Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+
+namespace Infrastructure.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        public async Task<Product> GetProductByIdAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IReadOnlyList<Product>> GetProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
