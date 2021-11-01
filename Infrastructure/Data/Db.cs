106364a0cac4f7ac6ec31adcc627ce41dbaa82a6
Kareem <75427112+KareemAzam@users.noreply.github.com>
2021-11-01 05:35:01 +0200

    Extending the products entity and creating related entities

--- Infrastructure/Data/Db.cs
+++ Infrastructure/Data/Db.cs
@@ -1,15 +1,16 @@
-﻿
-using Core.Entities;
+﻿using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
 {
-    public class Db:DbContext
+    public class Db : DbContext
     {
-        public Db(DbContextOptions options) :base(options)
+        public Db(DbContextOptions options) : base(options)
         {
         }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
     }
 }
