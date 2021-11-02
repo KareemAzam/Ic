d2e291961aa3e547c286e128cbb5bf69771314d8
Kareem <75427112+KareemAzam@users.noreply.github.com>
2021-11-02 06:21:30 +0200

    Serving static content from the API

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,6 +34,8 @@ namespace API
 
             app.UseRouting();
 
+            app.UseStaticFiles();
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
