f4d12105a82bf6d50208df2d5b29415e84fbb2dc
kareem <kareemreda85@gmail.com>
2021-11-21 00:57:43 +0200

    Adding breadcrumbs

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { MaterialModule } from '../material.module';
-import { ThemeComponent } from './theme/theme.component';
-import { RouterModule } from '@angular/router';
-import { TestErrorComponent } from './test-error/test-error.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { ServerErrorComponent } from './server-error/server-error.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {NavBarComponent} from './nav-bar/nav-bar.component';
+import {MaterialModule} from '../material.module';
+import {ThemeComponent} from './theme/theme.component';
+import {RouterModule} from '@angular/router';
+import {TestErrorComponent} from './test-error/test-error.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {ServerErrorComponent} from './server-error/server-error.component';
+import {SectionHeaderComponent} from './section-header/section-header.component';
 
 @NgModule({
   declarations: [
@@ -15,8 +16,10 @@ import { ServerErrorComponent } from './server-error/server-error.component';
     TestErrorComponent,
     NotFoundComponent,
     ServerErrorComponent,
+    SectionHeaderComponent,
   ],
   imports: [CommonModule, MaterialModule, RouterModule],
-  exports: [NavBarComponent],
+  exports: [NavBarComponent, SectionHeaderComponent],
 })
-export class CoreModule {}
+export class CoreModule {
+}
